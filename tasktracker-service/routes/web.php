b670bd52572cae1c823532f6b29682ca880494fc
funkylen <funkylen@gmail.com>
2022-10-16 16:26:52 +0300

    [HW-2] Реализация OAuth - получение информации о юзере

--- tasktracker-service/routes/web.php
+++ tasktracker-service/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+
+const OAUTH_CLIENT_ID = '97777af1-4c32-42c9-afeb-c4e23d66af9c';
+const OAUTH_CLIENT_SECRET = 'kiRbWEpI4lI3l2mvy4Pj7ScpkDWbcXqMURoyY53t';
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +33,38 @@ Route::group([
     Route::post('/reassign', 'reassignAll');
     Route::get('/{task}', 'show');
     Route::post('/{task}/complete', 'complete');
+});
+
+Route::get('/auth', function () {
+    $query = http_build_query([
+        'client_id' => OAUTH_CLIENT_ID,
+        'response_type' => 'code',
+    ]);
+
+    $url = 'http://localhost:8010/oauth/authorize';
+
+    return redirect($url . '?' . $query);
+})->name('auth');
+
+Route::get('/oauth/callback', function (Request $request) {
+    $base = 'http://auth-service_server_1';
+
+    $tokenResponse = Http::post($base . '/oauth/token', [
+        'grant_type' => 'authorization_code',
+        'client_id' => OAUTH_CLIENT_ID,
+        'client_secret' => OAUTH_CLIENT_SECRET,
+        'code' => $request->get('code'),
+    ]);
+
+    if ($tokenResponse->status() !== 200) {
+        return redirect('auth');
+    }
+
+    $data = $tokenResponse->json();
+
+    $token = $data['access_token'];
+
+    $userResponse = Http::withToken($token)->get($base . '/api/user');
+
+    dd($userResponse->json());
 });
